5ac2d6a1bb2e2b7dd2c09a9fa419e49ccb8a8e6a
madalenamarcelino <madelynbennet@gmail.com>
2019-04-12 16:20:31 +0100

    added class

---
+++ BookManager/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookManager
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+    }
+}
